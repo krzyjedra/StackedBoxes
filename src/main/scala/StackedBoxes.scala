4a549fe133752e839c0ec97d227e3fa2bf076775
k.jedra <kozukj@gmail.com>
2022-03-23 14:08:57 +0100

    initial commit

---
+++ src/main/scala/StackedBoxes.scala
@@ -0,0 +1,16 @@
+object StackedBoxes extends App {
+  val boxes = args.filter(_.length % 2 == 1).sortBy(_.length)
+  val widthStack = boxes.last.length
+  boxes.foreach(name => {
+    println(rowToString("-" * name.length, widthStack, '+'))
+    println(rowToString(name, widthStack, '|'))
+  })
+  println(rowToString("-" * widthStack, widthStack, '+'))
+
+
+  def rowToString(name: String, maxWidth: Int, separator: Char): String = {
+    val offset = (maxWidth - name.length) / 2
+    " " * offset + separator.toString + name + separator.toString + " " * offset
+  }
+
+}
